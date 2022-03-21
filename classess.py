58cc53c9c302b1b87fb3344b33cadea77154a0f3
Hamza Shabbir <hamzashabbir447@gmail.com>
2022-03-21 16:13:10 +0500

    Class Inheritence

---
+++ classess.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self,name,talk) -> None:
+        self.name=name
+        self.talk=talk
+
+Speaker1=Person('Sumama','LinkdIn')
+print(Speaker1.name)
+print(Speaker1.talk)
+Speaker2=Person('Adil','DevOps')
+print(Speaker2.name)
+print(Speaker2.talk)
+
+
+
--- digitToWords.py
+++ digitToWords.py
@@ -1,8 +1,13 @@
-
+num=input('Enter Number > ')
 phonebook={
     '1':'One',
     '2':'Two',
-    '3':'Three'
+    '3':'Three',
+    '4':'Four'
+
 }
+for i in num:
+    print( phonebook.get(i,'!!!!'))
 
 print(phonebook['1'])
+print(phonebook['2'])
