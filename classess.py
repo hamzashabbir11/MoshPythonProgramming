09f82509f49a5a4753f9d6cf1eafd76fb498c69e
Hamza Shabbir <hamzashabbir447@gmail.com>
2022-03-21 16:16:02 +0500

    change

--- classess.py
+++ classess.py
@@ -1,14 +1,19 @@
-class Person:
+class Developer:
     def __init__(self,name,talk) -> None:
         self.name=name
         self.talk=talk
 
-Speaker1=Person('Sumama','LinkdIn')
+Speaker1=Developer('Sumama','LinkdIn')
 print(Speaker1.name)
 print(Speaker1.talk)
-Speaker2=Person('Adil','DevOps')
+Speaker2=Developer('Adil','DevOps')
 print(Speaker2.name)
 print(Speaker2.talk)
 
 
+class DataScience(Developer):
+    pass
 
+datastudent1=DataScience('Anis','Towards Data Science ')
+print(datastudent1.name)
+print(datastudent1.talk)
